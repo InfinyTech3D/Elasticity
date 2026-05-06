f09363b6807513e97cda690125004ecc64804f00
Fimache <f.imache85@gmail.com>
2026-05-06 10:46:47 +0200

    Apply suggestion from @th-skam
    Co-authored-by: Themis Skamagkis <70031729+th-skam@users.noreply.github.com>

--- examples/Freefem/2D/mms_validation/mms_2d.py
+++ examples/Freefem/2D/mms_validation/mms_2d.py
@@ -175,7 +175,7 @@ class _QuadElement:
         """
         tmpl = _dim_template(dim)
 
-        rootNode.addObject("RequiredPlugin", name="Sofa.component.Visual")
+        rootNode.addObject("RequiredPlugin", pluginName="Sofa.Component.Visual")
         rootNode.addObject("RequiredPlugin", pluginName=[
             "Elasticity", "Sofa.Component.Constraint.Projective",
             "Sofa.Component.Engine.Select", "Sofa.Component.LinearSolver.Direct",
